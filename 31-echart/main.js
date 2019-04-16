dffa69c9e914aa45970acb13cf6e07e47099bb9e
chenchen210 <1486001914@qq.com>
2019-04-16 17:20:01 +0800

    echart

--- 31-echart/main.js
+++ 31-echart/main.js
@@ -1,39 +1,33 @@
 /*global echarts:true*/
-$(function(){
-  var myChart = echarts.init($('.main').get(0));
-  var xData=[],
-      yData=[];
 
-  for(var p=0;p<=1;p+=0.1){
-    xData.push(p);
-    yData.push(h(p));
-    console.log(xData,yData);
-  }
-
-  function h(p){
-    return -1 * p * Math.log2(p) - (1-p) * Math.log2(1-p);
-  }
-
-
-  var option = {
+$(function() {
+  for (var t, a = [], e = [], n = 0; n <= 1; n += .1)
+  a.push(o(n, 1)),
+  e.push(o(-1 * (r(t = n) + r(1 - t)), 2));
+  var i = {
     title: {
-      text: '二进熵函数曲线'        
+      text: "二进熵函数曲线"
     },
     tooltip: {},
     legend: {
-      data:['二进熵']    
+      data: ["信息量"]
     },
     xAxis: {
-      data: xData
+      data: a
     },
     yAxis: {},
     series: [{
-      name: '信息量',
-      type: 'line',
-      data: yData
+      name: "信息量",
+      type: "line",
+      smooth: "true",
+      data: e
     }]
-        
   };
-  myChart.setOption(option);
-
+  function o(t, a) {
+    return Math.round(t * Math.pow(10, a)) / Math.pow(10, a)
+  }
+  function r(t) {
+    return 0 === t ? 0 : t * Math.log2(t);
+  }
+  echarts.init($(".main").get(0)).setOption(i)
 });
