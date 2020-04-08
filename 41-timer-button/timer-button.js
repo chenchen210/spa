44e07c02b18b7428043139aabb2bcb096f7aa1ff
chenchen210 <1486001914@qq.com>
2020-04-08 22:28:42 +0800

    UI组件

---
+++ 41-timer-button/timer-button.js
@@ -0,0 +1,24 @@
+$(function(){
+
+  var n=6;
+  var $container=$('.main');
+  //模板块方式
+  var DOM ='<input class="timer-button" type="button" value="同意(6s)" disabled>';
+  $container.html(DOM);
+  var $btnAgree = $('input[type="button"]');
+  
+  var timer=window.setInterval(function(){
+    n--;
+    if(n === 0){
+      window.clearInterval(timer);
+      $btnAgree.removeAttr('disabled');
+      $btnAgree.val('同意');
+    }else{
+      $btnAgree.val('同意('+n+'s)');
+    }
+  },1000);
+
+  $btnAgree.click(function(){
+    alert('SPA! 10-success!');
+  });
+});
